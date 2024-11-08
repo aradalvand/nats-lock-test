810a6b67bb6e91db0d66aebe80db840ce421e006
Arad Alvand <aradalvand@gmail.com>
2024-11-08 17:05:33 +0330

    with canellation token

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ using NATS.Net;
 await using var client = new NatsClient();
 IDistributedLocker locker = new NatsDistributedLocker(client);
 Console.WriteLine("Trying...");
-await using (var handle = await locker.Acquire(args.First()))
+
+await using var handle = await locker.Acquire(args.First());
+if (handle is null) return;
+_ = Task.Run(async () =>
 {
-    if (handle is null) return;
-    Console.WriteLine("Became leader");
-    Console.ReadLine();
-}
+    while (true)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine($"Canceled? {handle.CancellationToken.IsCancellationRequested}");
+    }
+});
+Console.WriteLine("Became leader");
+Console.ReadLine();
 
 public interface IDistributedLocker
 {
@@ -18,16 +25,25 @@ public interface IDistributedLocker
     /// The lock has a TTL of 3 seconds and is renewed in the background every second. If the program crashes, the lock will be freed after at most 3 seconds, and can be acquired by another process.
     /// </summary>
     /// <param name="key">A string specifying a name that identifies the lock resource.</param>
-    /// <returns>An <see cref="IAsyncDisposable"/> that frees the lock when disposed.</returns>
-    Task<IAsyncDisposable> Acquire(string key);
+    /// <returns>An <see cref="ILockHandle"/> that frees the lock when disposed.</returns>
+    Task<ILockHandle> Acquire(string key);
 
     /// <summary>
     /// Tries to acquire a given lock; returns `null` if the lock isn't free immediately.
     /// The lock has a TTL of 3 seconds and is periodically renewed in the background until the lock is disposed. If the program crashes, the lock will be freed after at most 3 seconds, and can be acquired by another process.
     /// </summary>
     /// <param name="key">A string specifying a name that identifies the lock resource.</param>
-    /// <returns>An <see cref="IAsyncDisposable"/> that frees the lock when disposed, or `null` if the lock wasn't acquired.</returns>
-    Task<IAsyncDisposable?> TryAcquire(string key);
+    /// <returns>An <see cref="ILockHandle"/> that frees the lock when disposed, or `null` if the lock wasn't acquired.</returns>
+    Task<ILockHandle?> TryAcquire(string key);
+}
+
+public interface ILockHandle : IAsyncDisposable
+{
+    /// <summary>
+    /// If the lease isn't refreshed in time, the next time the refresh is attempted, we'll get an error,
+    /// which signals that the lock has been acquired by another process, we need to kill our process once this happens.
+    /// </summary>
+    CancellationToken CancellationToken { get; }
 }
 
 public class NatsDistributedLocker(
@@ -44,7 +60,7 @@ public class NatsDistributedLocker(
         })
         .AsTask();
 
-    public async Task<IAsyncDisposable?> TryAcquire(string key)
+    public async Task<ILockHandle> Acquire(string key)
     {
         var kv = await _kv;
         ulong rev;
@@ -53,47 +69,13 @@ public class NatsDistributedLocker(
             rev = await kv.CreateAsync(key, "");
 
             CancellationTokenSource cts = new();
-            _ = RefreshLock(cts.Token);
+            _ = Renew(cts);
             return new Lock(async () =>
             {
                 Console.WriteLine("Disposing...");
                 cts.Cancel();
                 await kv.DeleteAsync(key);
-            });
-        }
-        catch (Exception ex) when (ex is NatsKVCreateException or NatsKVWrongLastRevisionException)
-        {
-            return null;
-        }
-
-        async Task RefreshLock(CancellationToken ct)
-        {
-            while (!ct.IsCancellationRequested)
-            {
-                // var refreshFrequency = (int)Ttl.TotalMilliseconds / 3;
-                var refreshFrequency = 10_000;
-                await Task.Delay(refreshFrequency, ct);
-                rev = await kv.UpdateAsync(key, "", rev, cancellationToken: ct);
-            }
-        }
-    }
-
-    public async Task<IAsyncDisposable> Acquire(string key)
-    {
-        var kv = await _kv;
-        ulong rev;
-        try
-        {
-            rev = await kv.CreateAsync(key, "");
-
-            CancellationTokenSource cts = new();
-            _ = RefreshLock(cts.Token);
-            return new Lock(async () =>
-            {
-                Console.WriteLine("Disposing...");
-                cts.Cancel();
-                await kv.DeleteAsync(key);
-            });
+            }, cts.Token);
         }
         catch (Exception ex) when (ex is NatsKVCreateException or NatsKVWrongLastRevisionException)
         {
@@ -107,20 +89,64 @@ public class NatsDistributedLocker(
             throw new Exception("Couldn't acquire the lock.");
         }
 
-        async Task RefreshLock(CancellationToken ct)
+        async Task Renew(CancellationTokenSource cts)
         {
-            while (!ct.IsCancellationRequested)
+            while (!cts.Token.IsCancellationRequested)
             {
                 // var refreshFrequency = (int)Ttl.TotalMilliseconds / 3;
                 var refreshFrequency = 10_000;
-                await Task.Delay(refreshFrequency, ct);
-                rev = await kv.UpdateAsync(key, "", rev, cancellationToken: ct);
+                await Task.Delay(refreshFrequency, cts.Token);
+                try
+                {
+                    rev = await kv.UpdateAsync(key, "", rev, cancellationToken: cts.Token);
+                }
+                catch (NatsKVWrongLastRevisionException ex)
+                {
+                    Console.WriteLine($"update threw: {ex}");
+                    cts.Cancel();
+                }
             }
         }
     }
 
-    private class Lock(Func<ValueTask> free) : IAsyncDisposable
+    public async Task<ILockHandle?> TryAcquire(string key)
     {
+        throw new NotImplementedException();
+        // var kv = await _kv;
+        // ulong rev;
+        // try
+        // {
+        //     rev = await kv.CreateAsync(key, "");
+
+        //     CancellationTokenSource cts = new();
+        //     _ = RefreshLock(cts.Token);
+        //     return new Lock(async () =>
+        //     {
+        //         Console.WriteLine("Disposing...");
+        //         cts.Cancel();
+        //         await kv.DeleteAsync(key);
+        //     });
+        // }
+        // catch (Exception ex) when (ex is NatsKVCreateException or NatsKVWrongLastRevisionException)
+        // {
+        //     return null;
+        // }
+
+        // async Task RefreshLock(CancellationToken ct)
+        // {
+        //     while (!ct.IsCancellationRequested)
+        //     {
+        //         // var refreshFrequency = (int)Ttl.TotalMilliseconds / 3;
+        //         var refreshFrequency = 10_000;
+        //         await Task.Delay(refreshFrequency, ct);
+        //         rev = await kv.UpdateAsync(key, "", rev, cancellationToken: ct);
+        //     }
+        // }
+    }
+
+    private class Lock(Func<ValueTask> free, CancellationToken ct) : ILockHandle
+    {
+        public CancellationToken CancellationToken => ct;
         public ValueTask DisposeAsync() => free();
     }
 }
