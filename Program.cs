e7b8bed0793f58bdeea90b2c24ad969f2a2c3380
Arad Alvand <aradalvand@gmail.com>
2024-11-08 16:12:06 +0330

    .

--- Program.cs
+++ Program.cs
@@ -4,16 +4,30 @@ using NATS.Net;
 await using var client = new NatsClient();
 IDistributedLocker locker = new NatsDistributedLocker(client);
 Console.WriteLine("Trying...");
-await using (await locker.LockOrWait(args.First()))
+await using (var handle = await locker.TryAcquire(args.First()))
 {
+    if (handle is null) return;
     Console.WriteLine("Became leader");
     Console.ReadLine();
 }
 
 public interface IDistributedLocker
 {
-    Task<IAsyncDisposable> LockOrWait(string key);
-    Task<IAsyncDisposable> LockOrGiveUp(string key);
+    /// <summary>
+    /// Tries to acquire a given lock, if it fails, it waits until the lock is freed.
+    /// The lock has a TTL of 3 seconds and is renewed in the background every second. If the program crashes, the lock will be freed after at most 3 seconds, and can be acquired by another process.
+    /// </summary>
+    /// <param name="key">A string specifying a name that identifies the lock resource.</param>
+    /// <returns>An <see cref="IAsyncDisposable"/> that frees the lock when disposed.</returns>
+    Task<IAsyncDisposable> Acquire(string key);
+
+    /// <summary>
+    /// Tries to acquire a given lock; returns `null` if the lock isn't free immediately.
+    /// The lock has a TTL of 3 seconds and is renewed in the background every second. If the program crashes, the lock will be freed after at most 3 seconds, and can be acquired by another process.
+    /// </summary>
+    /// <param name="key">A string specifying a name that identifies the lock resource.</param>
+    /// <returns>An <see cref="IAsyncDisposable"/> that frees the lock when disposed, or `null` if the lock wasn't acquired.</returns>
+    Task<IAsyncDisposable?> TryAcquire(string key);
 }
 
 public class NatsDistributedLocker(
@@ -21,10 +35,17 @@ public class NatsDistributedLocker(
 ) : IDistributedLocker
 {
     private const string LockStoreName = "_locks";
+    private readonly Task<INatsKVStore> _kv = natsClient
+        .CreateKeyValueStoreContext()
+        .CreateStoreAsync(new NatsKVConfig(LockStoreName)
+        {
+            MaxAge = TimeSpan.FromSeconds(3),
+        })
+        .AsTask();
 
-    public async Task<IAsyncDisposable> LockOrGiveUp(string key)
+    public async Task<IAsyncDisposable?> TryAcquire(string key)
     {
-        var kv = await natsClient.CreateKeyValueStoreContext().CreateStoreAsync(LockStoreName);
+        var kv = await _kv;
         ulong rev;
         try
         {
@@ -41,22 +62,22 @@ public class NatsDistributedLocker(
         }
         catch (Exception ex) when (ex is NatsKVCreateException or NatsKVWrongLastRevisionException)
         {
-            throw new Exception("Lock couldn't be acquired.");
+            return null;
         }
 
         async Task RefreshLock(CancellationToken ct)
         {
             while (!ct.IsCancellationRequested)
             {
-                await Task.Delay(100_000_000, ct);
+                await Task.Delay(1_000, ct);
                 rev = await kv.UpdateAsync(key, "", rev, cancellationToken: ct);
             }
         }
     }
 
-    public async Task<IAsyncDisposable> LockOrWait(string key)
+    public async Task<IAsyncDisposable> Acquire(string key)
     {
-        var kv = await natsClient.CreateKeyValueStoreContext().CreateStoreAsync(LockStoreName);
+        var kv = await _kv;
         ulong rev;
         try
         {
@@ -75,10 +96,10 @@ public class NatsDistributedLocker(
         {
             await foreach (var entry in kv.WatchAsync<string>(key))
             {
-                if (entry.Operation is not NatsKVOperation.Del)
+                if (entry.Operation is not NatsKVOperation.Del) // todo: automatic deletion due to `maxage` doesn't result in a `del` so we won't be notified
                     continue;
 
-                return await LockOrWait(key);
+                return await Acquire(key);
             }
             throw new Exception("Couldn't acquire the lock.");
         }
@@ -87,7 +108,7 @@ public class NatsDistributedLocker(
         {
             while (!ct.IsCancellationRequested)
             {
-                await Task.Delay(100_000_000, ct);
+                await Task.Delay(1_000, ct);
                 rev = await kv.UpdateAsync(key, "", rev, cancellationToken: ct);
             }
         }
